42c7f2c84f1d312fd54540eee4953ca0e8467508
dinura-imesh <dinuraimesh@gmail.com>
2022-04-19 11:54:07 +0530

    Remove logs

--- src/modules/database.ts
+++ src/modules/database.ts
@@ -3,7 +3,6 @@ import Container from 'typedi';
 import { ContainerConstants } from '../constants';
 
 export const connectDB = async () => {
-  console.log(process.env.DB_PASS as string);
   const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USER as string, 'Nek123#@!', {
     host: process.env.DB_SERVER,
     logging: false,
